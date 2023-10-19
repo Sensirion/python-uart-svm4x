4aa1d56f80376fc698346413fa415fb8393d50bd
smueller <simon.mueller@sensirion.com>
2023-10-19 09:54:36 +0200

    remove old files

--- sensirion_shdlc_svm41/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import, division, print_function
-from .version import version as __version__  # noqa: F401
-
-from sensirion_shdlc_svm41.device import Svm41Device  # noqa: F401
-
-__all__ = ['Svm41Device']
